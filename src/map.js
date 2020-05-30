8b02567487e9069864ae0842c2ada0050babc368
1papaya <1love1papaya@gmail.com>
2020-05-30 18:32:33 +0100

    add loader

--- src/map.js
+++ src/map.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from "react";
-import MapGL, {
-  GeolocateControl,
-  FullscreenControl,
-  NavigationControl,
-  ScaleControl,
-  Source,
-  Layer,
-  Image,
-} from "@urbica/react-map-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
-import mapStyle from "./style/style.json";
 import gdvPin from "./style/gdvPin.png";
 
 import HoverPopup from "./components/hoverPopup";
 import SelectedPopup from "./components/selectedPopup";
 
-console.log(gdvPin);
+import Loader from "./components/loader";
 
 const Map = (props) => {
   let mapRef = React.createRef();
@@ -52,145 +42,186 @@ const Map = (props) => {
       });
   }, [selectedFeature]);
 
-  // for some reason have got to fire a fake loading s
+  // Lazy load map-gl and style
+  let [MapGL, setMapGL] = useState(null);
+  let [mapStyle, setMapStyle] = useState(null);
+  let [isMapLoaded, setIsMapLoaded] = useState(false);
+
   useEffect(() => {
-    mapRef.current.getMap().fire("load", { fake: true });
+    import(
+      /* webpackChunkName: "MapGL" */ "@urbica/react-map-gl"
+    ).then((MapGL) => setMapGL(MapGL));
+
+    import(
+      /* webpackChunkName: "mapStyle" */ "./style/style.json"
+    ).then((mapStyle) => setMapStyle(mapStyle));
   }, []);
+
+  // for some reason have got to fire a fake loading
+  // in order for controls to be displayed immediately
+  // let [fakeLoaded, setFakeLoaded] = useState(false);
+  // useEffect(() => {
+  //   if (mapStyle && MapGL && !fakeLoaded && mapRef.current) {
+  //       mapRef.current.getMap().fire("load", { fake: true });
+  //       setFakeLoaded(true);
+  //   }
+  // }, [mapStyle, MapGL]);
 
   let { data } = props;
 
   return (
-    <MapGL
-      ref={mapRef}
-      style={{ width: "100%", height: "100%" }}
-      onViewportChange={setViewport}
-      mapStyle={mapStyle}
-      onLoad={(e) => {
-        if (e && !"fake" in e) props.onLoad(e);
-      }}
-      transformRequest={(url) => {
-        // TODO remoooveee thisssss !!!! style json
-        if (url.indexOf("/") === -1) return; // fix webpack-dev-server
+    <div style={{ width: "100%", height: "100%" }}>
+      {!isMapLoaded && <Loader />}
+      {MapGL && mapStyle && (
+        <MapGL.default
+          ref={mapRef}
+          style={{ width: "100%", height: "100%" }}
+          onViewportChange={setViewport}
+          mapStyle={mapStyle}
+          onLoad={(e) => {
+            if (e && !"fake" in e) setIsMapLoaded(true);
+          }}
+          transformRequest={(url) => {
+            // TODO remoooveee thisssss !!!! style json
+            if (url.indexOf("/") === -1) return; // fix webpack-dev-server
 
-        let url_ = new URL(url);
-        let loc = window.location;
+            let url_ = new URL(url);
+            let loc = window.location;
 
-        if (url.search("//localhost") != -1)
-          return {
-            url: `${loc.origin}${loc.pathname}${url_.pathname}`,
-          };
-      }}
-      {...viewport}
-    >
-      <Image id="gdvPin" image={gdvPin} />
+            if (url.search("//localhost") != -1)
+              return {
+                url: `${loc.origin}${loc.pathname}${url_.pathname}`,
+              };
+          }}
+          {...viewport}
+        >
+          <MapGL.Image id="gdvPin" image={gdvPin} />
 
-      {/* Sources */}
-      <Source id="gdv_tracks" type="geojson" data={data.tracks}></Source>
-      <Source id="gdv_updates" type="geojson" data={data.updates}></Source>
-      <Source id="gdv_waypoints" type="geojson" data={data.waypoints}></Source>
+          {/* Sources */}
+          <MapGL.Source
+            id="gdv_tracks"
+            type="geojson"
+            data={data.tracks}
+          ></MapGL.Source>
+          <MapGL.Source
+            id="gdv_updates"
+            type="geojson"
+            data={data.updates}
+          ></MapGL.Source>
+          <MapGL.Source
+            id="gdv_waypoints"
+            type="geojson"
+            data={data.waypoints}
+          ></MapGL.Source>
 
-      {/* Waypoints */}
-      <Layer
-        id="gdv_waypoints"
-        type="symbol"
-        source="gdv_waypoints"
-        before="gdv_waypoints-placeholder"
-        layout={{
-          "icon-image": "dot_11",
-        }}
-      />
+          {/* Waypoints */}
+          <MapGL.Layer
+            id="gdv_waypoints"
+            type="symbol"
+            source="gdv_waypoints"
+            before="gdv_waypoints-placeholder"
+            layout={{
+              "icon-image": "dot_11",
+            }}
+          />
 
-      {/* Tracks */}
-      <Layer
-        id="gdv_tracks-casing"
-        type="line"
-        source="gdv_tracks"
-        before="gdv_tracks-placeholder"
-        paint={{
-          "line-color": "#fff",
-          "line-width": 5,
-        }}
-      />
-      <Layer
-        id="gdv_tracks"
-        type="line"
-        source="gdv_tracks"
-        before="gdv_tracks-placeholder"
-        paint={{
-          "line-color": "#d37aff",
-          "line-width": 2,
-        }}
-      />
+          {/* Tracks */}
+          <MapGL.Layer
+            id="gdv_tracks-casing"
+            type="line"
+            source="gdv_tracks"
+            before="gdv_tracks-placeholder"
+            paint={{
+              "line-color": "#fff",
+              "line-width": 5,
+            }}
+          />
+          <MapGL.Layer
+            id="gdv_tracks"
+            type="line"
+            source="gdv_tracks"
+            before="gdv_tracks-placeholder"
+            paint={{
+              "line-color": "#d37aff",
+              "line-width": 2,
+            }}
+          />
 
-      {/* Updates */}
-      <Layer
-        id="gdv_updates"
-        type="symbol"
-        source="gdv_updates"
-        layout={{
-          "icon-image": "gdvPin",
-          "icon-anchor": "bottom",
-          "icon-size": ["case", ["==", ["get", "last"], true], 0.4, 0.2],
-          "icon-allow-overlap": true,
-        }}
-      />
+          {/* Updates */}
+          <MapGL.Layer
+            id="gdv_updates"
+            type="symbol"
+            source="gdv_updates"
+            layout={{
+              "icon-image": "gdvPin",
+              "icon-anchor": "bottom",
+              "icon-size": ["case", ["==", ["get", "last"], true], 0.4, 0.2],
+              "icon-allow-overlap": true,
+            }}
+          />
 
-      {/* Refuges */}
-      <Layer
-        id="pyr_refuges"
-        type="symbol"
-        source="pyr"
-        source-layer="refuges"
-        onClick={(r) => {
-          setHoveredFeature(null);
-          setSelectedFeature(r.features[0]);
-        }}
-        onHover={(r) => {
-          let feat = r.features[0];
+          {/* Refuges */}
+          <MapGL.Layer
+            id="pyr_refuges"
+            type="symbol"
+            source="pyr"
+            source-layer="refuges"
+            onClick={(r) => {
+              setHoveredFeature(null);
+              setSelectedFeature(r.features[0]);
+            }}
+            onHover={(r) => {
+              let feat = r.features[0];
 
-          if (
-            selectedFeature &&
-            feat.properties.id === selectedFeature.properties.id
-          )
-            return;
+              if (
+                selectedFeature &&
+                feat.properties.id === selectedFeature.properties.id
+              )
+                return;
 
-          setHoveredFeature(feat);
-          r.target.getCanvas().style.setProperty("cursor", "pointer");
-        }}
-        onLeave={(r) => {
-          setHoveredFeature(null);
-          r.target.getCanvas().style.removeProperty("cursor");
-        }}
-        layout={{
-          "icon-image": {
-            stops: [
-              [0, ""],
-              [10, "shelter_11"],
-            ],
-          },
-          "icon-size": {
-            stops: [
-              [10, 0.5],
-              [12, 1],
-            ],
-          },
-        }}
-      />
+              setHoveredFeature(feat);
+              r.target.getCanvas().style.setProperty("cursor", "pointer");
+            }}
+            onLeave={(r) => {
+              setHoveredFeature(null);
+              r.target.getCanvas().style.removeProperty("cursor");
+            }}
+            layout={{
+              "icon-image": {
+                stops: [
+                  [0, ""],
+                  [10, "shelter_11"],
+                ],
+              },
+              "icon-size": {
+                stops: [
+                  [10, 0.5],
+                  [12, 1],
+                ],
+              },
+            }}
+          />
 
-      {/* Popup */}
-      {hoveredFeature && <HoverPopup feature={hoveredFeature} />}
-      {selectedFeature && (
-        <SelectedPopup
-          feature={selectedFeature}
-          onClose={() => setSelectedFeature(null)}
-        />
+          {/* Popup */}
+          {hoveredFeature && <HoverPopup feature={hoveredFeature} />}
+          {selectedFeature && (
+            <SelectedPopup
+              feature={selectedFeature}
+              onClose={() => setSelectedFeature(null)}
+            />
+          )}
+
+          {/* Controls */}
+          <MapGL.NavigationControl showCompass showZoom position="top-left" />
+          <MapGL.FullscreenControl position="top-left" />
+          <MapGL.ScaleControl
+            maxWidth={100}
+            unit="metric"
+            position="bottom-left"
+          />
+        </MapGL.default>
       )}
-
-      {/* Controls */}
-      <NavigationControl showCompass showZoom position="top-left" />
-      <FullscreenControl position="top-left" />
-      <ScaleControl maxWidth={100} unit="metric" position="bottom-left" />
-    </MapGL>
+    </div>
   );
 };
 
